7acd830f6cc83e43f42b5ce010c32b310437abc3
sushil phuyal <87110634+1337mickey@users.noreply.github.com>
2022-12-01 15:35:01 +0545

    Add files via upload

---
+++ web/cookie/index.php
@@ -0,0 +1,10 @@
+<?php
+    setcookie("flag", "cookie_is_tasty", time()+3600);
+   
+    echo '<center><img src="cookie.jpg" /><center>';
+    echo"";
+    echo"";
+    echo"";
+     echo "<h1><center>find my cookie</center></h1>";
+
+?>
